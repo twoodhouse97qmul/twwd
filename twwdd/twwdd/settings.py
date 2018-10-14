6e74c83b0fd757a0e6cfafbaac1970e7e6ed8347
twoodhouse97qmul <aoterop97@gmail.com>
2018-10-14 09:50:36 +0100

    fc

--- twwdd/twwdd/settings.py
+++ twwdd/twwdd/settings.py
@@ -119,17 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(
-        os.path.dirname(__file__),
-        'static',
-    ),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
